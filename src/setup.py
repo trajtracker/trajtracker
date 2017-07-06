4aba7ee4c019822b4b8b6e3c93e2d08f44b8bf86
droralef <dror.dotan@gmail.com>
2017-07-06 12:20:35 +0300

    Moved paradigms from here to a separate git repository

--- src/setup.py
+++ src/setup.py
@@ -1,12 +1,9 @@
 from setuptools import setup, find_packages
 from os import listdir
 
-res_directories = 'res/images', 'res/sounds'
-data_files = [(dir_name, [dir_name + "/" + filename for filename in listdir(dir_name)]) for dir_name in res_directories]
-
 
 setup(name='trajtracker',
-      version='0.4.1',
+      version='1.0.0',
       description='Framework for creating trajectory-tracking experiments',
       url='http://trajtracker.com',
       author='Dror Dotan',
@@ -21,5 +18,4 @@ setup(name='trajtracker',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3.6'
       ],
-      data_files=data_files,
       zip_safe=False)
