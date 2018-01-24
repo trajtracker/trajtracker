f196505321ad9e1d011887f48df65eaa67f16300
droralef <dror.dotan@gmail.com>
2018-01-24 19:19:17 +0200

    Added comment

--- src/create_distribution.sh
+++ src/create_distribution.sh
@@ -5,3 +5,7 @@ rm -rf dist trajtracker.egg-info build
 python setup.py bdist_wheel --universal
 
 rm -rf trajtracker.egg-info build
+
+# After the distribution was tested, upload it to PyPi by writing:
+# twine upload <distribution_file_name>
+
